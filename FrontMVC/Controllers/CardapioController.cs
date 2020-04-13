65abb2eaf78e0c2be427c6349f5d15b5aa706466
Guilherme Almeida <guilherme.almeida@onlineapp.com.br>
2020-04-13 09:20:50 -0300

    Criação das controllers e views

---
+++ FrontMVC/Controllers/CardapioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontMVC.Controllers
+{
+    public class CardapioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
